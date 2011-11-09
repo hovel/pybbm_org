b37cb006195d20fa783e73d66e3f1d59f2fe527d
zeus <gelios@gmail.com>
2011-11-09 14:53:08 +0300

    initial setup

---
+++ urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls.defaults import patterns, include, url
+
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    url(r'^admin/', include(admin.site.urls)),
+    (r'^', include('pybb.urls', namespace='pybb')),
+    (r'^accounts/', include('registration.urls')),
+
+)
