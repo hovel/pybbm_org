998aea1e23f7ee70ccf09fdac50eb882414854fb
Sergey Fursov <GeyseR85@gmail.com>
2013-11-04 12:14:36 +0400

    amazon ses requirments, fixed fabfile, added auto deploy via circleci

--- fabfile.py
+++ fabfile.py
@@ -9,27 +9,19 @@ from fabric.api import *
 PROJECT_NAME = 'pybbm_org'
 PROJECT_PROCESS = 'pybbm'
 
-PROJECT_BASEDIR = '/home/zeus/webapps/%s' % PROJECT_PROCESS
-PROJECT_ROOT = '/home/zeus/webapps/%s/%s'% (PROJECT_PROCESS, PROJECT_NAME)
+PROJECT_BASEDIR = '/home/geyser/%s' % PROJECT_NAME
+PROJECT_ROOT = '/home/geyser/%s/%s' % (PROJECT_NAME, PROJECT_NAME)
 PROJECT_SOURCE = 'git@github.com:hovel/%s.git' % PROJECT_NAME
-#noinspection PyRedeclaration
-env.hosts = ['zeus@pybbm.org']
+env.hosts = ['geyser@ec2-50-17-136-53.compute-1.amazonaws.com']
 
-
-def install():
-    with cd(PROJECT_BASEDIR):
-        run('git clone %s' % PROJECT_SOURCE)
-        run('virtualenv env')
-        run('pip-2.7 -E env install -r %s/build/pipreq.txt' % PROJECT_NAME)
 
 def fu():
-    local('./manage.py test pybb')
     with cd(PROJECT_ROOT):
         run('git fetch')
-        run('git checkout origin/master')
-        run('pip -E ../env install pybbm --upgrade --no-deps')
-        run('pip -E ../env install -r build/pipreq.txt')
-        run('../env/bin/python manage.py syncdb')
-        run('../env/bin/python manage.py migrate')
-        run('../env/bin/python manage.py collectstatic --noinput')
-        run('/home/zeus/webapps/%s/gunicorn.sh restart' % PROJECT_PROCESS)
+        run('git pull origin master')
+        run('git checkout master')
+        run('%s/env/bin/pip install -r %s/build/pipreq.txt -U' % (PROJECT_BASEDIR, PROJECT_ROOT))
+        run('%s/env/bin/python %s/manage.py syncdb' % (PROJECT_BASEDIR, PROJECT_ROOT) )
+        run('%s/env/bin/python %s/manage.py migrate' % (PROJECT_BASEDIR, PROJECT_ROOT))
+        run('%s/env/bin/python %s/manage.py collectstatic --noinput' % (PROJECT_BASEDIR, PROJECT_ROOT))
+        run('kill -HUP `cat %s/log/pidfile`' % PROJECT_BASEDIR)
