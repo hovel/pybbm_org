e75d87dead8f6d297dbe9c407978809c1979386e
Sergey Fursov <GeyseR85@gmail.com>
2013-11-06 23:05:24 +0400

    10 sec delay between search crawls

--- pybbm_org/urls.py
+++ pybbm_org/urls.py
@@ -3,11 +3,14 @@ from django.conf.urls import patterns, include, url
 from django.conf.urls.static import static
 from django.conf import settings
 from django.contrib import admin
+from django.views.generic.base import TemplateView
 from pybbm_org.forms import PybbmRegistrationForm
 
 admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = patterns(
+    '',
+    url(r'^robots.txt$', TemplateView.as_view(template_name='robots.txt')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('pybb.urls', namespace='pybb')),
     url(r'^accounts/signup/$', SignupView.as_view(form_class=PybbmRegistrationForm), name='registration_register'),
