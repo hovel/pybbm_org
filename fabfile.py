7031ab3f1114ead8605f5ba409ac9ea32f9d9774
Sergey Fursov <GeyseR85@gmail.com>
2014-01-08 01:32:40 +0400

    files for auto deploy

--- fabfile.py
+++ fabfile.py
@@ -1,28 +1,109 @@
 #!/usr/bin/env python
-# vim:fileencoding=utf-8
-
-__author__ = 'zeus'
-
 from fabric.api import *
-
+import urllib
+import urllib2
+import os
+try:
+    from statictv.settings import CLOUDFLARE_TOKEN, CLOUDFLARE_EMAIL
+except ImportError:
+    CLOUDFLARE_TOKEN = None
+    CLOUDFLARE_EMAIL = None
 
 PROJECT_NAME = 'pybbm_org'
-PROJECT_PROCESS = 'pybbm'
+HOME = '/home/zeus'
+PROJECT_SOURCE = 'git@github.com:hovel/pybbm_org.git'
+PROJECT_BASEDIR = '%s/%s' % (HOME, PROJECT_NAME)
+PYTHON_PATH = '%s/.virtualenvs/%s/bin/python' % (HOME, PROJECT_NAME)
+PIP_PATH = '%s/.virtualenvs/%s/bin/pip' % (HOME, PROJECT_NAME)
+env.use_ssh_config = True
+env.sudo_user = 'zeus'
 
-PROJECT_BASEDIR = '/home/geyser/%s' % PROJECT_NAME
-PROJECT_ROOT = '/home/geyser/%s/%s' % (PROJECT_NAME, PROJECT_NAME)
-PROJECT_SOURCE = 'git@github.com:hovel/%s.git' % PROJECT_NAME
-env.hosts = ['geyser@ec2-107-20-93-189.compute-1.amazonaws.com']
+all_hosts = []
+i = 1
+while True:
+    host = os.environ.get('HOST%s' % i)
+    if host:
+        all_hosts.append(host)
+    else:
+        break
+    i += 1
+env.hosts = all_hosts
 
 
-def fu():
-    with cd(PROJECT_ROOT):
-        run('git fetch')
-        run('git pull origin master')
-        run('git checkout master')
-        run('%s/env/bin/pip install -r %s/build/pipreq.txt -U' % (PROJECT_BASEDIR, PROJECT_ROOT))
-        run('%s/env/bin/python %s/manage.py syncdb' % (PROJECT_BASEDIR, PROJECT_ROOT))
-        run('%s/env/bin/python %s/manage.py migrate captcha' % (PROJECT_BASEDIR, PROJECT_ROOT))
-        run('%s/env/bin/python %s/manage.py migrate pybb' % (PROJECT_BASEDIR, PROJECT_ROOT))
-        run('%s/env/bin/python %s/manage.py collectstatic --noinput' % (PROJECT_BASEDIR, PROJECT_ROOT))
-        run('sudo kill -HUP `cat %s/log/pidfile`' % PROJECT_BASEDIR)
+def uninstall():
+    with cd(HOME):
+        sudo('rm -rf %s' % PROJECT_BASEDIR)
+
+
+def install():
+    with cd(HOME):
+        sudo('mkvirtualenv %s' % PROJECT_NAME)
+        sudo('git clone %s %s' % (PROJECT_SOURCE, PROJECT_NAME))
+        sudo('touch %s/logs/%s.log' % (HOME, PROJECT_NAME))
+        sudo('touch %s/logs/%s-error.log' % (HOME, PROJECT_NAME))
+    put_settings()
+    copy_nginx_config()
+    update()
+    start()
+
+
+def update():
+    with cd(PROJECT_BASEDIR):
+        sudo('git pull')
+        sudo('%s install -r build/pipreq.txt -U' % PIP_PATH)
+        sudo('%s manage.py syncdb' % PYTHON_PATH)
+        sudo('%s manage.py migrate' % PYTHON_PATH)
+        sudo('%s manage.py syncdb --all' % PYTHON_PATH)
+        sudo('%s manage.py collectstatic --noinput' % PYTHON_PATH)
+        sudo('./gunicorn.sh reload')
+    purge_clouflare_static()
+
+
+def start():
+    with cd(PROJECT_BASEDIR):
+        sudo('./gunicorn.sh start')
+
+
+def stop():
+    with cd(PROJECT_BASEDIR):
+        sudo('./gunicorn.sh stop')
+
+
+def restart():
+    with cd(PROJECT_BASEDIR):
+        sudo('./gunicorn.sh restart')
+
+
+def reload():
+    with cd(PROJECT_BASEDIR):
+        sudo('./gunicorn.sh reload')
+
+
+def put_settings():
+    put('conf/local.py', '%s/pybbm_org/settings_local.py' % PROJECT_BASEDIR, use_sudo=True, temp_dir='/home/zeus/tmp')
+    with settings(sudo_user='root'):
+        sudo('chown zeus:zeus %s/pybbm_org/settings_local.py' % PROJECT_BASEDIR)
+
+
+def copy_nginx_config():
+    with settings(sudo_user='root'), cd(PROJECT_BASEDIR):
+        sudo('cp conf/pybbm_org.conf /etc/nginx/conf.d/pybbm_org.conf')
+        sudo('/etc/init.d/nginx restart')
+
+
+def purge_clouflare_static():
+    token = os.environ.get('CLOUDFLARE_TOKEN', CLOUDFLARE_TOKEN)
+    email = os.environ.get('CLOUDFLARE_EMAIL', CLOUDFLARE_EMAIL)
+    if not token or not email:
+        print 'purge static files failed'
+        return
+
+    response = urllib2.urlopen('https://www.cloudflare.com/api_json.html',
+                               data=urllib.urlencode({
+                                   'a': 'fpurge_ts',
+                                   'tkn': token,
+                                   'email': email,
+                                   'z': 'pybbm.org',
+                                   'v': '1'
+                               }))
+    print response.read()
