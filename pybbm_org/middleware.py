280ea4f0b4f79c78070beebc7f5fe7906e6155b7
Sergey Fursov <GeyseR85@gmail.com>
2014-07-29 19:09:39 +0400

    fixed remote addr middleware

--- pybbm_org/middleware.py
+++ pybbm_org/middleware.py
@@ -6,4 +6,11 @@ class RemoteAddrMiddleware(object):
     def process_request(self, request):
         remote_addr = request.META.get('REMOTE_ADDR')
         if not remote_addr or remote_addr == '127.0.0.1':
-            request.META['REMOTE_ADDR'] = request.META.get('HTTP_X_FORWARDED_FOR', remote_addr)
+            forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+            if not forwarded_for:
+                return
+            try:
+                forwarded_for.split(',')[0].strip()
+                request.META['REMOTE_ADDR'] = forwarded_for
+            except:
+                pass
