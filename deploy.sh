68215ebe3e3b2bfef30908e8879533b0a5e6c1c2
Sergey Fursov <GeyseR85@gmail.com>
2014-10-22 18:20:56 +0400

    fixed path again

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo $3 > pass
-ansible-playbook -i ~/deploy-inventory/ansible/inventory/production_mini/hosts site_content/build/deploy.yml --vault-password-file pass -e "@build/vars/production.yml"
+ansible-playbook -i ~/deploy-inventory/ansible/inventory/production_mini/hosts build/deploy.yml --vault-password-file pass -e "@build/vars/production.yml"
