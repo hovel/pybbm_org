4eafac7203d5c2d67ab907dff2de1f72c17f920d
Sergey Fursov <GeyseR85@gmail.com>
2014-10-28 14:10:17 +0300

    deployment fixes

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 echo $3 > pass
+cp ~/deploy-inventory/ansible/playbooks/deploy_app.yml build/deploy.yml
 ansible-playbook -i ~/deploy-inventory/ansible/inventory/production_mini/hosts build/deploy.yml --vault-password-file pass -e "@build/vars/production.yml"
