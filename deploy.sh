06bea8e79e7350216741ef6bebb68520523cc202
lampslave <lampslave@gmail.com>
2017-04-26 02:41:39 +0300

    new deploy

--- deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo $3 > pass
-cp ~/deploy-inventory/ansible/playbooks/deploy_app.yml build/deploy.yml
-ansible-playbook -i ~/deploy-inventory/ansible/inventory/production_mini/hosts build/deploy.yml --vault-password-file pass -e "@build/vars/master.yml" -e hovel_repo_name=$1 -e hovel_branch_name=$2
